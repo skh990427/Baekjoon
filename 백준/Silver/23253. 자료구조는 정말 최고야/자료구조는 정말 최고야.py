b9b04e906e8ede4fd90fe4fdca63f92d95b5f861
skh990427 <84136901+skh990427@users.noreply.github.com>
2022-11-22 14:18:44 +0900

    [Silver V] Title: 자료구조는 정말 최고야, Time: 300 ms, Memory: 52340 KB -BaekjoonHub

--- 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -2,14 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-p = True
-for _ in range(m):
-    i = int(input())
-    k = list(map(int, input().split()))
-    for j in range(i-1):
-        if k[j] < k[j+1]:
-            p = False
-            break
-    if not p: break
+flag = True
 
-print('Yes' if p else 'No') 
+for _ in range(m):
+    num = int(input())
+    l = list(map(int, input().split()))
+    for i in range(num-1):
+        if l[i] < l[i+1]:
+            flag = False
+            break
+    if not flag:
+        break
+if flag:
+    print('Yes')
+else:
+    print('No')
