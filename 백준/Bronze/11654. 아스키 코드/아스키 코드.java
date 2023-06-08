d1f81a368bc02f101d97f3dc78df8239759d5494
KwangHo <84136901+skh990427@users.noreply.github.com>
2023-06-09 00:32:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int num = stdIn.next().charAt(0);
+
+        System.out.println(num);
+    }
+}
