9ef0143383dccfe3df90f6a933f5d2fb9e606ef2
KwangHo <84136901+skh990427@users.noreply.github.com>
2023-06-28 12:03:48 +0900

    [Silver IV] Title: 스택, Time: 460 ms, Memory: 17012 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            String command = token.nextToken();
+
+            if (command.equals("push")) {
+                stack.push(Integer.parseInt(token.nextToken()));
+            } else if (command.equals("pop")) {
+                if (!stack.isEmpty()) {
+                    System.out.println(stack.pop());
+                } else {
+                    System.out.println(-1);
+                }
+            } else if (command.equals("size")) {
+                System.out.println(stack.size());
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (command.equals("top")) {
+                if (!stack.isEmpty()) {
+                    System.out.println(stack.peek());
+                } else {
+                    System.out.println(-1);
+                }
+            }
+        }
+    }
+}
